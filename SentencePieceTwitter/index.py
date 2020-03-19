45acfd8e3baec853425767d4b37680618223ec14
carban <murillo.carlos@correounivalle.edu.co>
2020-03-19 15:38:27 -0500

    token by words and prefixes and file chooser

--- SentencePieceTwitter/index.py
+++ SentencePieceTwitter/index.py
@@ -1,38 +1,66 @@
 from GUI.gui import *
+
 import sentencepiece as sp
-
-from PyQt5.QtWidgets import QFileDialog
-
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 class myGUI(QtWidgets.QMainWindow, Ui_MainWindow):
 	def __init__(self, *args, **kwargs):
 		QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
 		self.setupUi(self)
-		self.modelPath = "Model/m_word.model"
+		self.modelWordPath = 'Model/m_word.model'
+		self.modelPrefixesPath = 'Model/m_user.model'
+		self.modelPath = self.modelWordPath
+		self.sel = True
 		
 		#My method
 		self.pushButton.clicked.connect(self.letsTknzr)
 		
+		#Combo box method
+		self.comboBox.currentIndexChanged.connect(self.selectionchange)
+		
 		#Open File
-		#self.pushButton_3.clicked.connect(self.openFile)
+		self.pushButton_3.clicked.connect(self.openFile)
 		
 	def letsTknzr(self):
-		#--- Charging model Sentence Piece ---
-		spWord = sp.SentencePieceProcessor()
-		spWord.load(self.modelPath)
 		
-		tkzed = str(spWord.encode_as_pieces(self.plainTextEdit.toPlainText()))
-		self.plainTextEdit_2.setPlainText(tkzed)
+		#--- Charging model Sentence Piece ---
+		spp = sp.SentencePieceProcessor()
+		spp.load(self.modelPath)
+		
+		tkzed = spp.encode_as_pieces(self.plainTextEdit.toPlainText())
+		output = self.cleanFormat(tkzed)
+		self.plainTextEdit_2.setPlainText(output)
+		
+	def cleanFormat(self, tkzed):
+		for i in range(len(tkzed)):
+			tkzed[i] = tkzed[i].replace("▁", " ")
+		
+		aux = str(tkzed)
+		aux = aux.replace("', '", " | ")
+		return aux
 
-	"""
+	def selectionchange(self, i):
+		
+		if(i == 0):
+			self.modelPath = self.modelWordPath
+			self.pushButton_3.setText("Change Model \nWords")
+			self.sel = True
+		else:
+			self.modelPath = self.modelPrefixesPath
+			self.pushButton_3.setText("Change Model \nPrefixes")
+			self.sel = False
+	
 	def openFile(self):
-		print(QtWidgets)
-		name = QtWidgets.QfileDialog.getOpenFileName(self, 'Open File')
-		file = open(name, 'r')
-		with file:
-			text = file.read()
-			print(text)
-	"""
+		
+		Tk().withdraw()
+		filename = askopenfilename()
+		if filename:
+			if(self.sel):
+				self.modelWordPath = filename
+			else:
+				self.modelPrefixesPath = filename
+			self.modelPath = filename
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
